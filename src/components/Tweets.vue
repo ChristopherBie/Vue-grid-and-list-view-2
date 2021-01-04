301576b1f604a2213fed5858398cd35a12b89b5f
Chris <newuser@News-MBP.hitronhub.home>
2021-01-04 01:16:01 -0700

    W13B

---
+++ src/components/Tweets.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <button @click='notifyParent'>display tweets</button>
+        <br>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "vue-tweets",
+        data() {
+            return {
+                tweets: [
+                    {
+                        author: 'James',
+                        date: 'Dec 10',
+                        text: 'Hello world!'
+                    },
+                    {
+                        author: 'Chris',
+                        date: 'Dec 11',
+                        text: 'HELLLOOO!!!'
+                    },
+                    {
+                        author: 'Everyone',
+                        date: 'Dec 12',
+                        text: 'You are muted!'
+                    }
+                ]
+            }
+        },
+        methods: {
+            notifyParent: function() {
+                this.$emit('tweetContent', this.tweets);
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
